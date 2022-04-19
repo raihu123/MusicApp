06b03c6da535c7f83eadd25dfeb55ba397661815
rayhanrashid <12345678asdf>
2022-04-18 23:17:11 -0500

    backend logout success

--- controller/authenticationController.js
+++ controller/authenticationController.js
@@ -1,23 +1,41 @@
 const User = require("../model/user");
 const basicAuth = require("basic-auth");
+
+let loggedInUsers = [];
+
+
 exports.login = (req, res, next) => {
   const user = basicAuth(req);
   console.log(user.name, user.pass);
   const allUser = User.fetchAll();
   const authentication = allUser.find(x => x.username === user.name && x.password === user.pass) ;
   if (authentication){
-    session=req.session;
-    session.userid=req.body.username;
-    res.status(200).json(`{"authentication" : "success"}`);
+    const userid = user.name + '|' + new Date().toLocaleString();
+    loggedInUsers.push(userid);
+    res.status(200).json({
+      "authentication" : "success",
+      "userid" : userid
+    });
   }else {
-    res.status(401).json(`{"authentication" : "failed"}`);
+    res.status(401).json({"authentication" : "failed"});
   }
 
 }
 
 exports.logout = (req, res, next) => {
-  req.session.destroy();
-  res.redirect('/');
+  console.log(loggedInUsers);
+  userid = req.header('userid');
+  index = loggedInUsers.findIndex(x => x === userid);
+  console.log(index);
+  if (index > -1) {
+    loggedInUsers.splice(index, 1);
+    console.log(loggedInUsers);
+    res.status(200).json({"msg" : "logout success"});
+  }else{
+    loggedInUsers = [];
+    console.log(loggedInUsers);
+    res.status(500).json({"msg" : "something went wrong!"});
+  }
 }
 
 
